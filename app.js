b5c7c4fc322388fdb56f88b6eb10331146603d76
AlexsanderDiego <alexdiegoat@gmail.com>
2024-03-14 00:59:07 -0300

    Adiciona rotas de links ao servidor

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ import home from "./routes/home.js";
 import server from "./config/server.js";
 import { authLogin } from "./routes/auth-login-routes.js";
 import { usuarioRoutes } from "./routes/usuario-routes.js";
+import { linksRoutes } from "./routes/links-routes.js";
 
 server.use(home);
 server.use(authLogin);
 server.use(usuarioRoutes);
+server.use(linksRoutes);
