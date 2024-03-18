d63e44f1a24b12e1887d7899034e64686949b6df
AlexsanderDiego <alexdiegoat@gmail.com>
2024-03-18 10:27:21 -0300

    Adiciona importação do pacote "cors" e utiliza-o no servidor

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import home from "./routes/home.js";
 import server from "./config/server.js";
+import cors from "cors";
 import { authLogin } from "./routes/auth-login-routes.js";
 import { usuarioRoutes } from "./routes/usuario-routes.js";
 import { linksRoutes } from "./routes/links-routes.js";
 
+server.use(cors)
 server.use(home);
 server.use(authLogin);
 server.use(usuarioRoutes);
