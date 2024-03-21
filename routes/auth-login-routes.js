ce0a16927cf406454f93022f0e14f30a5d1c469d
AlexsanderDiego <alexdiegoat@gmail.com>
2024-03-21 00:28:06 -0300

    Atualizar rota de login com encriptação de senha

--- routes/auth-login-routes.js
+++ routes/auth-login-routes.js
@@ -1,30 +1,30 @@
-import { prisma } from '../config/db.js';
+import { prisma } from "../config/db.js";
 import { Router } from "express";
 import bcrypt from "bcrypt"; // Adicionando o encript de senha
 
 const authLogin = Router();
 
 authLogin.post("/auth/login", async (req, res) => {
-    const { email, senha } = req.body;
-    console.log('email', email);
-    console.log('password', senha);
+  const { email, senha } = req.body;
+  console.log("email", email);
+  console.log("password", senha);
 
-    const emailUser = await prisma.usuarios.findUnique({
-        where: {
-            email: email,
-        },
-    });
-    if (!emailUser) {
-        return res.json({ error: 'Usuário não encontrado' });
-    }
-    const passwordMatch = await bcrypt.compare(senha, emailUser.senha);
-    if (!passwordMatch) {
-        return res.json({ error: 'Senha incorreta' });
-    }
+  const emailUser = await prisma.usuarios.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  if (!emailUser) {
+    return res.json({ error: "Usuário não encontrado" });
+  }
+  const passwordMatch = await bcrypt.compare(senha, emailUser.senha);
+  if (!passwordMatch) {
+    return res.json({ error: "Senha incorreta" });
+  }
 
-    delete emailUser.senha;
-    res.json(emailUser);
-    console.log('Usuário logado com sucesso');
+  delete emailUser.senha;
+  res.json(emailUser);
+  console.log("Usuário logado com sucesso");
 });
 
 export { authLogin };
