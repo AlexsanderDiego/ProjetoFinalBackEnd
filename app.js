6de80af433cc04c71c19e5f10fccb70c1bf81660
AlexsanderDiego <alexdiegoat@gmail.com>
2024-03-27 23:57:46 -0300

    Atualizar dependências do Express e importar o servidor corretamente

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-import express from "express";
-import cors from "cors";
-import 'dotenv/config';
-
-const server = express();
-
-server.use(cors());
-server.use(express.json());
-
 import home from "./routes/home.js";
-// import server from "./config/server.js";
+import server from "./config/server.js";
 import { authLogin } from "./routes/auth-login-routes.js";
 import { usuarioRoutes } from "./routes/usuario-routes.js";
 import { linksRoutes } from "./routes/links-routes.js";
@@ -17,7 +8,3 @@ server.use(home);
 server.use(authLogin);
 server.use(usuarioRoutes);
 server.use(linksRoutes);
-
-server.listen(3000, () => {
-    console.log("Servidor está rodando");
-  });
